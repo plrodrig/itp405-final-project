77994deb4c224003b43936a3f4ac6f8d212edb5b
Priscilla Rodriguez <priscillarodriguez@guest-wireless-upc-1607-10-120-125-126.usc.edu>
2016-04-28 20:19:55 -0700

    Register form CSS ready

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -3,6 +3,12 @@
 <head>
   <meta charset="UTF-8">
   <title>Register</title>
+  <!-- Latest compiled and minified CSS -->
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+  <script type="text/javascript" src="{{ URL::asset('js/registerJS.js') }}"></script>
+  <link rel="stylesheet" href="{{ URL::asset('css/registerCSS.css') }}" />
+
 </head>
 <body>
   @yield('content')
